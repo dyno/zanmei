c783282f4afadd5f5edfc4d04fc26beb1d0aa6eb
Dyno Fu <dyno.fu@gmail.com>
2019-03-04 11:16:02 -0800

    Rewrite zanmei with aiohttp
    also use traditional chinese as title

--- zanmei.py
+++ zanmei.py
@@ -2,17 +2,18 @@
 
 # vim: set fileencoding=utf-8 :
 
+import asyncio
 import re
-import shutil
 from pathlib import Path
-from typing import Dict
+from typing import List
 
 import attr
-import requests
 from absl import app, logging as log
 from bs4 import BeautifulSoup
+from hanziconv import HanziConv
 
-from comm import init_logging
+from aiohttp import ClientSession
+from comm import fetch, init_logging
 
 # 教會聖詩 Hymns for God's People
 HYMNS_INDEX_URL = "https://www.zanmeishi.com/songbook/hymns-for-gods-people.html"
@@ -32,31 +33,38 @@ class Hymn:
         self.filepath = DOWNLOAD / f"{self.no:03d}_{self.name}.png"
 
 
-def index(url: str) -> Dict[str, Hymn]:
-    r = requests.get(url)
-    soup = BeautifulSoup(r.text, "html.parser")
+async def index(session: ClientSession, url: str) -> List[Hymn]:
+    status, content = await fetch(session, url)
+    text = content.decode()
+    soup = BeautifulSoup(text, "html.parser")
     div = soup.find("div", attrs={"class": "sbtablist"})
+    hymns = []
     for li in div.findAll("li"):
         no = re.search(r"\d+", li.text).group()
         name = li.a["title"].replace("查看歌谱", "")
+        name = HanziConv.toTraditional(name)
         hymn = Hymn(name=name, no=int(no), url=f"{ZANMEI_HOMEPAGE}{li.a['href']}")
-        yield hymn
+        hymns.append(hymn)
+
+    return hymns
 
 
-def download(hymn: Hymn):
+async def download(session: ClientSession, hymn: Hymn):
     if hymn.filepath.exists():
-        log.debug(f"{hymn} is already downloaded.")
+        log.debug(f"{hymn.filepath} is already downloaded.")
         return
 
-    log.debug(f"download {hymn} ...")
-    r = requests.get(hymn.url)
-    soup = BeautifulSoup(r.text, "html.parser")
+    log.debug(f"downloading to {hymn.filepath} ...")
+    status, content = await fetch(session, hymn.url)
+    assert status == 200
+    text = content.decode()
+    soup = BeautifulSoup(text, "html.parser")
     div = soup.find("div", attrs={"class": "img_tab"})
     img_url = div.a["href"]
-    r = requests.get(img_url, stream=True)
-    assert r.status_code == 200
+    status, content = await fetch(session, img_url)
+    assert status == 200
     with hymn.filepath.open("wb") as f:
-        shutil.copyfileobj(r.raw, f)
+        f.write(content)
 
 
 def verify(filepath: Path, glob: str, total: int):
@@ -71,17 +79,25 @@ def verify(filepath: Path, glob: str, total: int):
     assert len(path_list) == total
 
 
+async def download_image_copy():
+    async with ClientSession() as session:
+        hymns = await index(session, HYMNS_INDEX_URL)
+        for hymn in hymns:
+            try:
+                await download(session, hymn)
+            except Exception:
+                log.exception(f"failed to download {hymn}.")
+
+        verify(DOWNLOAD, "*.png", len(hymns))
+
+
 def main(argv):
     del argv
     DOWNLOAD.mkdir(exist_ok=True, parents=True)
     init_logging()
-    hymns = list(index(HYMNS_INDEX_URL))
-    for hymn in hymns:
-        try:
-            download(hymn)
-        except Exception:
-            log.exception(f"failed to download {hymn}.")
-    verify(DOWNLOAD, "*.png", len(hymns))
+
+    loop = asyncio.get_event_loop()
+    loop.run_until_complete(download_image_copy())
 
 
 if __name__ == "__main__":
