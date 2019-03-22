f529cc0eeda41203ccbbc67d4bb1ba4608904a80
Dyno Fu <dyno.fu@gmail.com>
2019-03-21 22:41:56 -0700

    correct index bible

--- scripture.py
+++ scripture.py
@@ -1,7 +1,10 @@
 #!/usr/bin/env python3
-
+import re
+import warnings
+from collections import OrderedDict, namedtuple
 from functools import lru_cache
 from pathlib import Path
+from typing import Dict, List
 
 import pandas as pd
 from absl import app, flags, logging as log
@@ -29,18 +32,111 @@ def to_record(f):
 def scripture(source=None):
     if source is None:
         source = FLAGS.bible_source
-    with Path(source).open() as f:
-        next(f)  # get rid of the first line
-        g = to_record(f)
-        return pd.DataFrame.from_records(g, columns=["book", "chapter", "verse", "scripture"]).set_index(
-            ["book", "chapter", "verse"]
+    # https://stackoverflow.com/questions/17912307/u-ufeff-in-python-string
+    with Path(source).open(encoding="utf-8-sig") as f:
+        df = pd.DataFrame.from_records(to_record(f), columns=["book", "chapter", "verse", "scripture"])
+        df.set_index(["book", "chapter", "verse"], inplace=True)
+        # if 2 verses merged to 1, use the 1st verse and clear the 2nd.
+        warnings.filterwarnings("ignore", category=pd.errors.PerformanceWarning)
+        df["scripture"] = df["scripture"].str.strip().str.replace("。", "。")
+        df.loc[df["scripture"].str.startswith("見上節"), ["scripture"]] = ""
+
+        return df
+
+
+ScriptureIndex = namedtuple("ScriptureIndex", ["book", "chapter", "verses"])
+
+
+def parse_locations(locations: str) -> Dict[str, ScriptureIndex]:
+    """
+    >>> from pprint import pprint
+    >>> pprint(parse_locations("撒母耳記上 17:31-49")) # 2019-02-24 #doctest: +NORMALIZE_WHITESPACE
+    OrderedDict([('撒母耳記上17:31-49',
+              ScriptureIndex(book='撒母耳記上', chapter='17', verses=['31', '32', '33', '34', '35', '36', '37', '38', '39', '40', '41', '42', '43', '44', '45', '46', '47', '48', '49']))])
+    >>> pprint(parse_locations("哥林多前書 6:12-13;10:23-24、31")) # 2019-03-03 #doctest: +NORMALIZE_WHITESPACE
+    OrderedDict([('哥林多前書6:12-13',
+              ScriptureIndex(book='哥林多前書', chapter='6', verses=['12', '13'])),
+             ('哥林多前書10:23-24,31',
+              ScriptureIndex(book='哥林多前書', chapter='10', verses=['23', '24', '31']))])
+    >>> pprint(parse_locations("約翰壹書 2:20, 24-27")) # 2019-03-10 #doctest: +NORMALIZE_WHITESPACE
+    OrderedDict([('約翰壹書2:20,24-27',
+              ScriptureIndex(book='約翰壹書', chapter='2', verses=['20', '24', '25', '26', '27']))])
+    >>> pprint(parse_locations("馬可福音 10:32-45")) # 2019-03-17 #doctest: +NORMALIZE_WHITESPACE
+    OrderedDict([('馬可福音10:32-45',
+              ScriptureIndex(book='馬可福音', chapter='10', verses=['32', '33', '34', '35', '36', '37', '38', '39', '40', '41', '42', '43', '44', '45']))])
+    >>> pprint(parse_locations("馬太福音 25：14-30")) # 2019-03-24 #doctest: +NORMALIZE_WHITESPACE
+    OrderedDict([('馬太福音25:14-30',
+              ScriptureIndex(book='馬太福音', chapter='25', verses=['14', '15', '16', '17', '18', '19', '20', '21', '22', '23', '24', '25', '26', '27', '28', '29', '30']))])
+    """
+    result = OrderedDict()
+
+    loc_list = re.split(r"[;；]", locations)
+    prev_book = None
+    for loc in loc_list:
+        # normalize location <book>chapter:verse-verse,verse;chapter:verse
+        loc = (
+            loc.replace("～", "-")
+            .replace("－", "-")
+            .replace("，", ",")
+            .replace("、", ",")
+            .replace("：", ":")
+            .replace("　", "")
+            .replace(" ", "")
         )
+
+        # 1. book
+        # use 約翰一書 not 約翰1書
+        m = re.search(r"^(?P<book>[^0-9 ]+)\s*", loc)
+        if m:
+            book = m.group("book")
+            prev_book = book
+            chapter_verses = loc[len(book) :]
+        else:
+            assert prev_book is not None
+            book = prev_book
+            chapter_verses = loc
+            loc = book + chapter_verses
+
+        # 2. chapter
+        # 11:12-15,19
+        parts = chapter_verses.strip().split(":")
+        chapter = parts[0]
+        ranges = parts[1]
+
+        # 3. verses
+        verses = []
+        for r in ranges.split(","):
+            parts = r.split("-")
+            if len(parts) == 1:
+                verses.append(parts[0])
+            else:
+                l, r = map(int, parts)
+                verses.extend([str(i) for i in range(l, r + 1)])
+
+        result[loc] = ScriptureIndex(book, chapter, verses)
+
+    return result
+
+
+def search(bible: pd.DataFrame, locations: str) -> Dict[str, List[str]]:
+    loc_list = parse_locations(locations)
+    result = OrderedDict()
+    for loc, scripture_index in loc_list.items():
+        df = bible.loc[scripture_index]
+        result[loc] = df
+
+    return result
 
 
 def main(argv):
     del argv
-    df = scripture()
-    print(df.loc[("約翰福音", "3", "16")]["scripture"])
+    bible = scripture()
+    with pd.option_context("display.unicode.east_asian_width", True, "display.max_colwidth", 200):
+        print(bible.loc[("約翰福音", "3", "16")]["scripture"])
+        result = search(bible, "馬太福音 25：14-31")
+        for loc, verses in result.items():
+            print(loc)
+            print(verses)
 
 
 if __name__ == "__main__":
